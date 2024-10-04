659b52f8692bedb4b7a16e232a7e9570836778d8
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2024-10-04 22:10:45 +0300

    check there is a questions in database

--- controller/questionController.js
+++ controller/questionController.js
@@ -3,8 +3,14 @@ const dbConnection = require("../db/dbConfig.js");
 const AllQuestions = async (req, res) => {
   try {
     const selectAllQuestions = "select * from questions";
-
     const response = await dbConnection.query(selectAllQuestions);
+        if (response[0].length === 0) {
+          return res.status(404).json({
+            error: "Not Found",
+            message: "No questions found.",
+          });
+        }
+
     res.status(200).json(response[0]);
 
     console.log(response[0]);
